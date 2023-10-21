f193350f4b462256a738bbbbc92a6ef13714a62e
Anna Lukianova <ann.lookyanova@gmail.com>
2023-10-21 19:51:58 +0200

    Extract tiles to separate file

---
+++ src/tiles.ts
@@ -0,0 +1,10 @@
+import Bear from "/src/assets/emojis/bear.png";
+import Dog from "/src/assets/emojis/dog.png";
+import Fox from "/src/assets/emojis/fox.png";
+import Frog from "/src/assets/emojis/frog.png";
+import Kitty from "/src/assets/emojis/kitty.png";
+import Mouse from "/src/assets/emojis/mouse.png";
+import Pig from "/src/assets/emojis/pig.png";
+import Unicorn from "/src/assets/emojis/unicorn.png";
+
+export const TILES = [Bear, Dog, Fox, Frog, Kitty, Mouse, Pig, Unicorn];
